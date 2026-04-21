be14527e2231898385712ee8afd269da0e188646
Patrik Svoboda <svobopat@fit.cvut.cz>
2026-04-21 12:25:52 +0200

    messing with DB configs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
     url:
       process.env.DATABASE_URL ??
-      'postgres://postgres:postgres@localhost:5432/printers',
+      "postgres://postgres:postgres@localhost:5432/printers",
+    ssl: { rejectUnauthorized: false },
   },
-})
+  // Tell drizzle-kit to only manage your own tables
+  tablesFilter: ["!pg_*", "!_timescaledb_*"],
+  schemaFilter: ["public"],
+});
