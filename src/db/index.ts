f82e6621ee442ff1f885c42785ff11d0c419165e
Patrik Svoboda <svobopat@fit.cvut.cz>
2026-04-30 12:17:35 +0200

    accept self signed cert while connecting

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,5 +5,7 @@ import * as schema from './schema'
 const connectionString =
   process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5432/printers'
 
-const client = postgres(connectionString)
+const client = postgres(connectionString, {
+  ssl: { rejectUnauthorized: false },
+})
 export const db = drizzle(client, { schema })
